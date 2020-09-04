5671391ebb0056eb4e3ae6619284fd9746abe56e
matsuo <g.matsuo@cosmowinds.jp>
2020-09-04 15:42:32 +0900

    メッセージの表示非表示

---
+++ WebSystem_Unity/Assets/T/Script/GoBoothCol.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoBoothCol : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject message;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //message = GameObject.FindGameObjectWithTag("GoBooth_Message");
+        //message.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            message.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            message.SetActive(false);
+        }
+    }
+
+}
